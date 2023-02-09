f585776222991538e3226d24f7e6d2a2da4c0102
Maksym <maksymRozhko7@gmail.com>
2023-02-09 11:28:48 +0200

    header, mobile menu, adaptive

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,5 +1,5 @@
 // import initSliders from './components/initSliders';
 // import tabs from './components/tabs';
-// import headerScrollFixed from './components/fixedHeader';
+import headerScrollFixed from './components/fixedHeader';
 // import scrollToTop from './components/scrollToTop';
 import customScripts from './components/customScripts';
