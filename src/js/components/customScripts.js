f685e35954249e12124be1fa43e7046cb446fe9a
Maksym <maksymRozhko7@gmail.com>
2023-02-15 11:43:44 +0200

    tabs, widget, edit region view

--- src/js/components/customScripts.js
+++ src/js/components/customScripts.js
@@ -17,4 +17,32 @@ document.addEventListener('DOMContentLoaded', () => {
             regionsDescription.classList.toggle('text-hidden')
         });
     }
+
+    const customTabs = (btnSelectors, contentSelectors) => {
+        const btns = document.querySelectorAll(btnSelectors);
+        const contents = document.querySelectorAll(contentSelectors);
+
+
+        const removeTabActiveClass = () => btns.forEach(btn => btn.parentElement.classList.remove('active'));
+        const removeContentsActiveClass = () => contents.forEach(content => content.classList.add('d-none'));
+
+        if (btns) {
+            btns.forEach((btn, i) => {
+
+                btn.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    if (!contents[i]) return;
+                    if (btn.dataset.type === contents[i].id) {
+                        removeTabActiveClass();
+                        removeContentsActiveClass();
+
+                        btn.parentElement.classList.add('active');
+                        contents[i].classList.remove('d-none');
+                    }
+                });
+            });
+        }
+    }
+
+    customTabs('.yachts .yachts__links a', '.yachts .hot-offers .hot-offer');
 });
